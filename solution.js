eac429842cbbabffd10db4871eb03975bc999e87
Rahul Jaiswal <rahul.jaiswal1997@gmail.com>
2022-07-06 23:01:10 +0530

    completed

--- solution.js
+++ solution.js
@@ -1,6 +1,13 @@
 // complete the given function
 
 function palindrome(str){
+let len=str.length;
 
+for(let i=0;i<len/2;i++){
+    if(str[i]!=str[len-i-1]){
+        return false;
+    }
+}
+return true;
 }
 module.exports = palindrome
